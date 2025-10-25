e095fe83dfe3663e35d93a48d1ade92fbbec2a81
unknown <mehedytouhidurzaman@gmail.com>
2025-10-25 13:19:09 +0600

    The basic setup of redux and axios is completed

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './routers/index.tsx'
 import { ThemeProvider } from './providers/theme.provider.tsx'
-
+import { Provider as ReduxProvider } from 'react-redux'
+import { store } from './redux/store.ts'
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme='system'>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <ReduxProvider store={store}>
+      <ThemeProvider defaultTheme="system">
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </ReduxProvider>
   </StrictMode>
 );
