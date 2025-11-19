019e3f44010654bc036c2b08031ed1fdea2d27b5
unknown <mehedytouhidurzaman@gmail.com>
2025-11-20 02:45:32 +0600

    search and filter is completed in the my rides history page is completed

--- src/components/modules/Dashboard/RidePage/MyRideTable.tsx
+++ src/components/modules/Dashboard/RidePage/MyRideTable.tsx
@@ -15,35 +15,53 @@ import { RideTimelineItems } from "@/constants/ride"
 import { toast } from "sonner"
 import Loader from "@/components/layouts/Loader"
 import { cn } from "@/lib/utils"
+import SearchAndFilterBar from "./SearchAndFilterBar"
+import { useSearchParams } from "react-router"
 
 
 export function MyRideTable() {
-    const totalItemsPerPage = 1
-
+    const totalItemsPerPage = 5
     const [currentPage, setCurrentPage] = useState(1)
-    const { data, isLoading: ridesInfoLoading } = useGetAllRidesQuery({ size: totalItemsPerPage, page: currentPage })
+
+    const [searchParams] = useSearchParams()
+
+   
+    const searchTerm = searchParams.get("searchTerm") 
+    const sortByDate = searchParams.get("sortByDate")
+    const rideStatus = searchParams.get("ride-status")
+    
+
+    const query = { 
+        size: totalItemsPerPage, 
+        page: currentPage, 
+        searchTerm,
+        sortByDate,
+        rideStatus
+    }
+
+
+    const { data, isLoading: ridesInfoLoading } = useGetAllRidesQuery(query)
 
     const [getPaymentUrl, { isLoading: getPaymentUrlLoading }] = useGetPaymentUrlMutation()
 
-    
+
 
     const ridesInfo = data?.data?.allRides
     const totalNumberOfItems = data?.data?.meta?.numberOfTotalRides || 0
-    
+
 
     const [open, setOpen] = useState(false)
     const [selectedRideInfo, setSelectedRideInfo] = useState<IRide | null>(null)
     const [defaultValueIndex, setDefaultValueIndex] = useState(0)
-    
-    const [paginateArray, setPaginateArray] = useState<number[]>([])
-   
 
-    
+    const [paginateArray, setPaginateArray] = useState<number[]>([])
+
+
+
 
     useEffect(() => {
-        
-        // const totalNumberOfItems = 5
-        
+
+
         const totalNumberOfPages = Math.ceil(totalNumberOfItems / totalItemsPerPage)
 
         if (totalNumberOfPages > 0) {
@@ -53,7 +71,7 @@ export function MyRideTable() {
             setPaginateArray([])
         }
 
-        
+
     }, [ridesInfo, totalNumberOfItems])
 
 
@@ -87,20 +105,19 @@ export function MyRideTable() {
         }
     }
 
-    const handelPrevious = () =>{
-        setCurrentPage(currentPage-1)
+    const handelPrevious = () => {
+        setCurrentPage(currentPage - 1)
     }
-    const handelNext = () =>{
-        setCurrentPage(currentPage +1)
+    const handelNext = () => {
+        setCurrentPage(currentPage + 1)
     }
-
-    console.log(currentPage)
-
 
 
     return (
         <>
-            <Table>
+            <SearchAndFilterBar />
+
+            <Table className="">
                 <TableHeader>
                     <TableRow>
                         <TableHead className="w-[100px]">#</TableHead>
@@ -111,6 +128,7 @@ export function MyRideTable() {
                         <TableHead className="">Ride Status</TableHead>
                         <TableHead className="">Payment Method</TableHead>
                         <TableHead className="">Payment Status</TableHead>
+                        <TableHead className="">Ride Date</TableHead>
                         <TableHead className="text-center">Action</TableHead>
                     </TableRow>
                 </TableHeader>
@@ -130,7 +148,8 @@ export function MyRideTable() {
                                 "text-red-600 font-bold": rideInfo.ride_status === "Canceled",
                             })}>{rideInfo.ride_status}</TableCell>
                             <TableCell className="">{rideInfo.paymentMethod}</TableCell>
-                            <TableCell className="">{rideInfo?.payment ? rideInfo?.payment.paymentStatus : "No data"}</TableCell>
+                            <TableCell className="">{rideInfo?.payment ? rideInfo?.payment.paymentStatus : "Not Accept Ride"}</TableCell>
+                            <TableCell className="">{rideInfo?.createdAt ? new Date(rideInfo.createdAt).toLocaleString().split(",")[0] : ""}</TableCell>
                             <TableCell className="space-x-2">
                                 <Button onClick={() => handleShowDetails(rideInfo)} className="cursor-pointer">See Details</Button>
 
@@ -140,6 +159,12 @@ export function MyRideTable() {
 
                         </TableRow>
                     ))}
+
+                    {
+                        ridesInfo?.length === 0 && <TableRow>
+                            <TableCell className="text-center text-muted-foreground" colSpan={10} >No Data found.</TableCell>
+                        </TableRow>
+                    }
                 </TableBody>
                 <RideDetailsModal defaultValueIndex={defaultValueIndex} rideData={selectedRideInfo} open={open} setOpen={setOpen} />
             </Table>
@@ -147,9 +172,9 @@ export function MyRideTable() {
             <div className="flex justify-center items-center gap-3">
                 <Button disabled={currentPage === 1} onClick={handelPrevious} className="" variant="outline">Previous</Button>
                 {
-                    paginateArray.length > 0 && paginateArray.map(item => <Button onClick={()=>setCurrentPage(item+1)} className={cn("font-bold",{
-                        "border border-primary": item+1 === currentPage
-                    })} variant="outline">{item+1}</Button>
+                    paginateArray.length > 0 && paginateArray.map((item, index) => <Button key={index} onClick={() => setCurrentPage(item + 1)} className={cn("font-bold", {
+                        "border border-primary": item + 1 === currentPage
+                    })} variant="outline">{item + 1}</Button>
                     )
                 }
                 <Button className="" disabled={currentPage === paginateArray.length} onClick={handelNext} variant="outline">Next</Button>
---
+++ src/components/modules/Dashboard/RidePage/SearchAndFilterBar.tsx
@@ -0,0 +1,95 @@
+
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+
+import  { useEffect, useState } from "react";
+
+import { useSearchParams } from "react-router";
+
+
+export default function SearchAndFilterBar() {
+
+    const [searchTerm, setSearchTerm] = useState<string>("")
+
+    const [sortByDate, setSortByDate] = useState<string>("")
+    const [rideStatus, setRideStatus] = useState<string>("")
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    
+
+
+    useEffect(()=>{
+        if(searchParams.toString() !== ""){
+            setSearchParams({})
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
+
+    // console.log(`${date?.getFullYear()}-${date?.getMonth()}-${date?.getDay()}`)
+
+    
+    
+    const handelSearchChange= (value: string) =>{
+        const params = new URLSearchParams(searchParams)
+        params.set("searchTerm", value)
+        if(value===""){
+            params.delete("searchTerm")
+        }
+        setSearchParams(params)
+        setSearchTerm(value)
+    }
+
+    const handelSortChange = (sortByDate: string) =>{
+        const params = new URLSearchParams(searchParams)
+        params.set("sortByDate", sortByDate)
+        setSearchParams(params)
+        setSortByDate(sortByDate)
+    }
+
+    const handelRideStatusFilter = (rideStatus: string) =>{
+        const params = new URLSearchParams(searchParams)
+        params.set("ride-status", rideStatus)
+        setSearchParams(params)
+        setRideStatus(rideStatus)
+    }
+
+    
+    
+
+  return (
+    <div className="mb-12 flex justify-center items-center gap-5">
+        <div>
+              <Input defaultValue={searchTerm} onChange={(e) => handelSearchChange(e.target.value)} placeholder="Search" />
+        </div>
+        <div>
+              <Select defaultValue={sortByDate} onValueChange={(value) => handelSortChange(value)} >
+                  <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Order By Date" />
+                  </SelectTrigger>
+                  <SelectContent>
+                      <SelectItem value="asc">Ascending</SelectItem>
+                      <SelectItem value="dsc">Descending</SelectItem>
+                  </SelectContent>
+              </Select>
+              
+        </div>
+        <div>
+
+              <Select defaultValue={rideStatus} onValueChange={(value) => handelRideStatusFilter(value)} >
+                  <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Select Ride Status" />
+                  </SelectTrigger>
+                  <SelectContent>
+                      <SelectItem value="Requested">Requested</SelectItem>
+                      <SelectItem value="Accepted">Accepted</SelectItem>
+                      <SelectItem value="Picked_Up">Picked_Up</SelectItem>
+                      <SelectItem value="In_Transit">In_Transit</SelectItem>
+                      <SelectItem value="Completed">Completed</SelectItem>
+                      <SelectItem value="Canceled">Canceled</SelectItem>
+                  </SelectContent>
+              </Select>
+
+        </div>
+    </div>
+  )
+}
